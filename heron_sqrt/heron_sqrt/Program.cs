7c6f6fe39e5587ae2bfbb726a3ebe2cfceacd0f6
Mary Mott (InConsulting Inc.) <mott.maryt@gmail.com>
2016-03-14 14:05:57 -0700

    started heron square root project

---
+++ heron_sqrt/heron_sqrt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heron_sqrt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class SquareRoot
+    {
+
+        public SquareRoot(double guess)
+        {
+
+        }
+    }
+}

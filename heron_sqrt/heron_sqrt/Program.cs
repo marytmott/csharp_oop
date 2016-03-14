d0c5a33cf05a71d15cdd4720f4bca9e5b6e8e835
Mary Mott (InConsulting Inc.) <mott.maryt@gmail.com>
2016-03-14 16:31:19 -0700

    working on heron sqrt

--- heron_sqrt/heron_sqrt/Program.cs
+++ heron_sqrt/heron_sqrt/Program.cs
@@ -10,15 +10,45 @@ namespace heron_sqrt
     {
         static void Main(string[] args)
         {
+            double sr = Heron.SquareRoot(10);
+
         }
     }
 
-    class SquareRoot
+    class Heron
     {
 
-        public SquareRoot(double guess)
+        public static double SquareRoot(double num)
         {
+            double guess = num / 2;
+            double margin = 1;
+            Console.WriteLine("initial guess {0}", guess);
 
+            /// check if arg is type int
+            if (num.GetType() != typeof(double))
+            {
+                throw new ArgumentException("Argument must be a double type");
+            }
+
+            /// check if arg is positive
+            if (num < 1)
+            {
+                throw new ArgumentException("Argument must be a positive value");
+            }
+
+            if (guess * guess > num - margin && guess * guess < num + margin)
+            {
+                Console.WriteLine("guessed right! {0}", guess);
+                Console.ReadLine();
+                return guess;
+            }
+
+            /// reset guess to try diff number'
+
+            guess = (guess + num / guess) / 2;
+            
+            Console.WriteLine("calling again {0}", guess);
+            return Heron.SquareRoot(guess);
         }
     }
 }

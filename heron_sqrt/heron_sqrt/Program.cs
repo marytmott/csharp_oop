7b3da99060f298b9fbed58e0082955277a7062c0
Mary T. Mott <mott.maryt@gmail.com>
2016-03-14 17:57:58 -0700

    heron's sqrt completed

--- heron_sqrt/heron_sqrt/Program.cs
+++ heron_sqrt/heron_sqrt/Program.cs
@@ -21,8 +21,8 @@ namespace heron_sqrt
         public static double SquareRoot(double num)
         {
             double guess = num / 2;
-            double margin = 1;
-            Console.WriteLine("initial guess {0}", guess);
+            double margin = .0001;
+            //Console.WriteLine("initial guess {0}", guess);
 
             /// check if arg is type int
             if (num.GetType() != typeof(double))
@@ -36,19 +36,22 @@ namespace heron_sqrt
                 throw new ArgumentException("Argument must be a positive value");
             }
 
-            if (guess * guess > num - margin && guess * guess < num + margin)
+
+            /// reset guess to try diff number
+
+            while(true)
             {
-                Console.WriteLine("guessed right! {0}", guess);
-                Console.ReadLine();
-                return guess;
+                /// if guess is between the margins we want
+                if (guess * guess > num - margin && guess * guess < num + margin)
+                {
+                    Console.WriteLine("guessed right! {0}", guess);
+                    //Console.ReadLine();
+                    return guess;
+                }
+                /// or try again!
+                guess = (guess + num / guess) / 2;
+
             }
-
-            /// reset guess to try diff number'
-
-            guess = (guess + num / guess) / 2;
-            
-            Console.WriteLine("calling again {0}", guess);
-            return Heron.SquareRoot(guess);
         }
     }
 }

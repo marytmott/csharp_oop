80e64bfcef18ee6b007e0d809a2c5a654e440a04
Mary T. Mott <mott.maryt@gmail.com>
2016-03-15 10:20:43 -0700

    began program, offset method complete

---
+++ caesar_cipher/caesar_cypher/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caesar_cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            CaesarCipher testCipher = new CaesarCipher(alphabet);
+            string newAbet = testCipher.setOffset(5);
+            Console.WriteLine(newAbet);
+            Console.ReadLine();
+        }
+    }
+
+    class CaesarCipher
+    {
+        private string alphabet;
+        private string offsetAlphabet;
+
+        /// sets the offset alphabet
+        public string setOffset(int offsetAmount)
+        {
+            string newBeginning;
+            string newEnding;
+
+            newBeginning = alphabet.Substring(offsetAmount);
+            newEnding = alphabet.Substring(0, offsetAmount);
+            this.offsetAlphabet = newBeginning + newEnding;
+
+            return this.offsetAlphabet;
+        }
+
+        /// cypher a message
+        //public string cipher(string message)
+        //{
+        //    //string ciphered;
+
+
+        //}
+
+        /// decipher a message
+        //public string decipher(string message)
+        //{
+
+        //}
+        
+        public CaesarCipher(string alphabet)
+        {
+            this.alphabet = alphabet;
+        }
+    }
+}

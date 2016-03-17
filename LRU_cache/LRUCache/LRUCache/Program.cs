e4b615841adfe82c2f1f6517ea0f47540a785fa8
Mary T. Mott <mott.maryt@gmail.com>
2016-03-17 11:07:47 -0700

    began LRUCache class with generics and build comments

--- LRU_cache/LRUCache/LRUCache/Program.cs
+++ LRU_cache/LRUCache/LRUCache/Program.cs
@@ -13,8 +13,68 @@ namespace LRUCache
         }
     }
 
-    public class LRUCache
+    public class LRUCache<TKey, TValue> // do we need to add constraints?
     {
+        // fields
+        private Dictionary<TKey, TValue> _cachedItems;
+        private LinkedList<TValue> _sortedUseList;  // TValue represents the type of nodes
+        //private int _count = 0;
+
+        // LinkedListNode
+
+        // properties
+        public int Count { get; private set; }
+        public int Length { get; set; }
+        //public int Length2
+        //{ get { return _length; } private set { _length = value; } }
+
+        // constructor - instantiate with length
+        public LRUCache(int length)
+        {
+            this.Length = length;
+            this.Count = 0;
+        }
+        
+
+
+        // constructor #2 - construct with a new key-value pair and length
+        public LRUCache(int length, TKey key, TValue val)
+        {
+
+            // call separate function to do this?
+            LinkedListNode<TValue> node = new LinkedListNode<TValue>(val);
+            this._sortedUseList = new LinkedList<TValue>();
+            this._sortedUseList.AddFirst(node);
+            this._cachedItems = new Dictionary<TKey, TValue>();
+            this._cachedItems.Add(key, value);
+
+            this.Length = length;
+            this.Count = 1;
+
+            // check for max length of list
+            // check if 
+
+
+        }
+
+        /*
+        Linked list:
+            - value
+            - move if found
+            - replace pointers when moved
+            - remove last --(also remove dictionary key reference)
+            - add head
+
+        Dictionary:
+            - key (to search)
+            - get node
+
+            -- if not found
+                -- if too long, remove last
+                -- or add
+
+        */
+
         // fields
         // dictionary
         // length

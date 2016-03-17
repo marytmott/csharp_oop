539e2613be2949ca9552c4c08321a77763716f9e
Mary T. Mott <mott.maryt@gmail.com>
2016-03-16 17:22:40 -0700

    started LRU project

---
+++ LRU_cache/LRUCache/LRUCache/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRUCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class LRUCache
+    {
+        // dictionary
+        // value found by key reference
+        // elements are added to top of list
+        // needs fixed size
+
+        // constructor indicating max cap
+        // property indicating curr # of els
+        // three methods
+
+        public void Add
+        {
+
+        }
+
+        public bool TryGetValue()
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        public LRUCache()
+        {
+
+        }
+    }
+}
---
+++ LRU_cache/LRUCache/LRUCacheTests/LRUCacheTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LRUCache;
+
+namespace LRUCacheTests
+{
+    [TestClass]
+    public class LRUCacheTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        // tests to write:
+
+    }
+}

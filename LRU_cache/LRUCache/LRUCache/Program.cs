459e0232ff6d450eda13c7a7595a1b49be5765b6
Mary T. Mott <mott.maryt@gmail.com>
2016-03-17 12:00:21 -0700

    clear method complete, comments added for other w lrucache methods

--- LRU_cache/LRUCache/LRUCache/Program.cs
+++ LRU_cache/LRUCache/LRUCache/Program.cs
@@ -15,15 +15,11 @@ namespace LRUCache
 
     public class LRUCache<TKey, TValue> // do we need to add constraints?
     {
-        // fields
         private Dictionary<TKey, TValue> _cachedItems;
         private LinkedList<TValue> _sortedUseList;  // TValue represents the type of nodes
-        //private int _count = 0;
-
-        // LinkedListNode
 
         // properties
-        public int Count { get; private set; }
+        public int Count { get; private set; }  // number of items currently in the cache
         public int Length { get; set; }
         //public int Length2
         //{ get { return _length; } private set { _length = value; } }
@@ -31,31 +27,53 @@ namespace LRUCache
         // constructor - instantiate with length
         public LRUCache(int length)
         {
-            this.Length = length;
+            // check length
+            if (length < 2)
+            {
+                throw new ArgumentException("Length must be greater than 1.");
+            } else
+            {
+                this.Length = length;
+            }
             this.Count = 0;
         }
         
-
-
-        // constructor #2 - construct with a new key-value pair and length
-        public LRUCache(int length, TKey key, TValue val)
+        // add to cache
+        public void Add(TKey key, TValue val)
         {
 
-            // call separate function to do this?
-            LinkedListNode<TValue> node = new LinkedListNode<TValue>(val);
-            this._sortedUseList = new LinkedList<TValue>();
-            this._sortedUseList.AddFirst(node);
-            this._cachedItems = new Dictionary<TKey, TValue>();
-            this._cachedItems.Add(key, value);
+            // check for length
+            // check for existence
+            // make new
 
-            this.Length = length;
-            this.Count = 1;
 
-            // check for max length of list
-            // check if 
 
 
         }
+
+        // will look for item in the cache
+        private bool TryGetValue(TKey key, out TValue val)
+        {
+            // find
+
+            // if not found, add
+            // if length is max
+
+            // if found
+            // move
+
+        }
+
+        // clears the cache
+        private void Clear()
+        {
+            _cachedItems.Clear();
+            _sortedUseList.Clear();
+            this.Count = 0;
+        }
+
+
+
 
         /*
         Linked list:
@@ -88,24 +106,5 @@ namespace LRUCache
         // property indicating curr # of els
         // three methods
 
-        public void Add
-        {
-
-        }
-
-        public bool TryGetValue()
-        {
-
-        }
-
-        public void Clear()
-        {
-
-        }
-
-        public LRUCache()
-        {
-
-        }
     }
 }
